da5b266f30ecc4ae28b985ea54b452034b7cf57d
Prerana Jadhav <jprerana50@gmail.com>
2026-02-10 19:23:19 +0530

    updating leave form

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -132,7 +132,7 @@ SIMPLE_JWT = {
 }
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
 ]
 
 CORS_ALLOW_HEADERS=list(default_headers)
