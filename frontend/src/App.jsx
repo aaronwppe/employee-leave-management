1366c6395b6115d44bd2e4af5a6beb0bbcd98fce
Prerana Jadhav <jprerana50@gmail.com>
2026-02-11 13:48:04 +0530

    Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import AdminLayout from "./layouts/AdminLayout";
 import EmployeeLayout from "./layouts/EmployeeLayout";
 import SetPassword from "./pages/SetPassword";
+import Login from "./pages/Login"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<AdminLayout />} />
         <Route path="/employee" element={<EmployeeLayout />} />
         <Route path="/set-password" element={<SetPassword />} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
   );
 }
